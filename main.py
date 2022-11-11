0cede08b33681d8d128a48b94566f86c96b789b2
deytalytics <james_dey@hotmail.com>
2022-11-11 21:04:13 +0000

    Handles ability to read the metadata yml file, or the payload in CSV or JSON

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import urllib.request, json
-url = "http://127.0.0.1:8000/"
+format="json"
+type=""
+url = "http://127.0.0.1:8000/"+type+"?format="+format
 fp = urllib.request.urlopen(url)
 mybytes = fp.read()
 
 mystr = mybytes.decode("utf8")
 fp.close()
 
-parsed = json.loads(mystr)
-print(json.dumps(parsed,indent=4))
+if format=="json":
+    parsed = json.loads(mystr)
+    print(json.dumps(parsed,indent=4))
+else:
+    print(mystr)
